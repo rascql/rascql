8da83761ce81136675f2f4008e8f9ca22c039189
Philip L. McMahon <philip.l.mcmahon@gmail.com>
2015-06-13 21:40:22 -0700

    Create query execution `BidiFlow`

---
+++ src/main/scala/rascql/postgresql/stream/QueryExecution.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2015 Philip L. McMahon
+ *
+ * Philip L. McMahon licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package rascql.postgresql.stream
+
+import akka.stream._
+import akka.stream.scaladsl._
+import rascql.postgresql.protocol._
+
+/**
+ * Executes simple and extended (prepared) queries.
+ *
+ * {{{
+ *                      . . . . . . . . . . . .
+ *                      .                     .
+ *                      .   +-------------+   .
+ *                      .   |             |   .
+ *           SendQuery --> [i]  queries  [o] --> FrontendMessage
+ *                      .   |             |   .
+ *                      .   +-------------+   .
+ *                      .                     .
+ *                      .   +-------------+   .
+ *                      .   |             |   .
+ * Source[QueryResult] <-- [o]  results  [i] <-- BackendMessage
+ *                      .   |             |   .
+ *                      .   +-------------+   .
+ *                      .                     .
+ *                      . . . . . . . . . . . .
+ * }}}
+ *
+ * @author Philip L. McMahon
+ */
+object QueryExecution {
+
+  def apply(): BidiFlow[SendQuery, FrontendMessage, BackendMessage, Source[QueryResult, Unit], Unit] =
+    BidiFlow() { implicit b =>
+
+      val queries = b.add(Flow[SendQuery].transform(() => new SendQueryStage))
+      val results = b.add(Flow[BackendMessage].splitWhen(_.isInstanceOf[ReadyForQuery]).map(_.transform(() => new QueryResultStage)))
+
+      BidiShape(queries, results)
+    }
+
+}
